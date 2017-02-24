45b0f6d5d397e8f240679f697897750587902110
Troy Whiteley <troy@somany.us>
2017-02-23 17:08:23 -0800

    Fix slack

--- methods/init/slack.js
+++ methods/init/slack.js
@@ -1,11 +1,10 @@
 module.exports = {
   method(done) {
     const server = this;
-    let plugin = this.settings.app.plugins['hapi-slack'];
     const config = server.plugins.healthcheck.config;
 
     if (config.slack) {
-      plugin = Object.assign(plugin, { _enabled: true }, config.slack);
+      this.settings.app.plugins['hapi-slack'] = Object.assign(this.settings.app.plugins['hapi-slack'], config.slack);
     }
 
     done();
