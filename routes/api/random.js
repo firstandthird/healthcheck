d4b04eeee9db99258093952c79cb2a9a615eda8c
Troy Whiteley <troy@somany.us>
2017-02-27 15:29:08 -0800

    Add useragent

--- routes/api/random.js
+++ routes/api/random.js
@@ -2,9 +2,8 @@ exports.random = {
   method: 'GET',
   path: '/api/random',
   handler(request, reply) {
-    const date = Date.now();
     const codes = [200, 404, 500, 200];
     const code = codes[Math.floor(Math.random() * codes.length)];
-    reply({ statusCode: code }).code(code);
+    reply({ statusCode: code, headers: request.headers }).code(code);
   }
-}
+};
