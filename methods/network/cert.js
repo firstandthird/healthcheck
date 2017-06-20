fca90a0d1d27c8b66023082e0926ad91500fecd2
Chris <orthagonal1@gmail.com>
2017-06-19 22:45:09 -0500

    handle and log https errors correctly

--- methods/network/cert.js
+++ methods/network/cert.js
@@ -4,20 +4,18 @@ const https = require('https');
 module.exports = {
   method(data) {
     const server = this;
-    try {
-      https.get(data.url, (res) => {
-        const certInfo = res.socket.getPeerCertificate();
-        const expiresIn = new Date(certInfo.valid_to).getTime() - new Date().getTime();
-        const oneWeek = 1000 * 60 * 60 * 24 * 7;
-        if (expiresIn < oneWeek) {
-          server.methods.report(data, {
-            timestamp: Date.now(),
-            expiresIn
-          });
-        }
-      });
-    } catch (e) {
-      // do nothing
-    }
+    https.get(data.url, (res) => {
+      const certInfo = res.socket.getPeerCertificate();
+      const expiresIn = new Date(certInfo.valid_to).getTime() - new Date().getTime();
+      const oneWeek = 1000 * 60 * 60 * 24 * 7;
+      if (expiresIn < oneWeek) {
+        server.methods.report(data, {
+          timestamp: Date.now(),
+          expiresIn
+        });
+      }
+    }).on('error', (error) => {
+      server.log(['healthcheck', 'error', 'certificate expiration check'], { data, error });
+    });
   }
 };
