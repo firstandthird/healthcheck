ef8e24173aff5380b785cf58e2e93d5ab0f3f2b5
Chris <orthagonal1@gmail.com>
2017-06-19 22:52:48 -0500

    handle errors with the https response as well

--- methods/network/cert.js
+++ methods/network/cert.js
@@ -5,14 +5,18 @@ module.exports = {
   method(data) {
     const server = this;
     https.get(data.url, (res) => {
-      const certInfo = res.socket.getPeerCertificate();
-      const expiresIn = new Date(certInfo.valid_to).getTime() - new Date().getTime();
-      const oneWeek = 1000 * 60 * 60 * 24 * 7;
-      if (expiresIn < oneWeek) {
-        server.methods.report(data, {
-          timestamp: Date.now(),
-          expiresIn
-        });
+      try {
+        const certInfo = res.socket.getPeerCertificate();
+        const expiresIn = new Date(certInfo.valid_to).getTime() - new Date().getTime();
+        const oneWeek = 1000 * 60 * 60 * 24 * 7;
+        if (expiresIn < oneWeek) {
+          server.methods.report(data, {
+            timestamp: Date.now(),
+            expiresIn
+          });
+        }
+      } catch (error) {
+        server.log(['healthcheck', 'error', 'certificate expiration check'], { data, error });
       }
     }).on('error', (error) => {
       server.log(['healthcheck', 'error', 'certificate expiration check'], { data, error });
