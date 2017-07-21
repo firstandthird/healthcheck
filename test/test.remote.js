bad542846474dac62efb281007644b046b4e83fe
Chris <orthagonal1@gmail.com>
2017-07-21 00:06:57 -0500

    server.methods.report and cert tests

--- test/test.remote.js
+++ test/test.remote.js
@@ -206,3 +206,130 @@ tap.test('can match an error status code', (t) => {
     t.end();
   });
 });
+
+tap.test('server.methods.report reports info on certification failure', (t) => {
+  t.plan(4);
+  async.autoInject({
+    // set up a remote route that healthcheck can get its config from:
+    configServer(done) {
+      const configServer = new Hapi.Server();
+      configServer.connection({ port: 3000 });
+      configServer.route({
+        method: 'get',
+        path: '/confi',
+        handler(request, reply) {
+          reply(null, {
+            urls: []
+          });
+        }
+      });
+      configServer.start(() => {
+        done(null, configServer);
+      });
+    },
+    rapptor(done) {
+      process.env.RAPPTOR_CONFIG_URL = 'http://localhost:3000/confi';
+      const rapptor = new Rapptor();
+      rapptor.start(done);
+    },
+    setup(rapptor, done) {
+      server = rapptor[0];
+      return done(null, rapptor[0]);
+    },
+    report(setup, done) {
+      server.log = (tags, input) => {
+        t.notEqual(tags.indexOf('HTTPS Test'), -1, 'contains tag with name of task');
+        t.notEqual(tags.indexOf('certificate-expiration'), -1, 'contains "certificate-expiration" tag');
+        t.notEqual(input.message.startsWith('The SSL certificate for HTTPS Test expires in '), 'certificate expiration explains what it is warning about');
+      };
+      const data = {
+        name: 'HTTPS Test',
+        type: 'certification',
+        url: process.env.HEALTHCHECK_TEST_URL
+      };
+      const result = {
+        url: process.env.HEALTHCHECK_TEST_URL,
+        expireLimit: 86400000000,
+        expiresIn: 4199784696
+      };
+      server.methods.report(data, result);
+      done();
+    },
+    cleanConfig(configServer, report, done) {
+      configServer.stop(done);
+    },
+    cleanup(cleanConfig, done) {
+      server.stop(done);
+    }
+  }, (err, result) => {
+    t.equal(err, null);
+    t.end();
+  });
+});
+
+tap.test('can handle cert warnings', (t) => {
+  t.plan(2);
+  const testResults = {};
+  async.autoInject({
+    // set up a remote route that healthcheck can get its config from:
+    configServer(done) {
+      const configServer = new Hapi.Server();
+      configServer.connection({ port: 3000 });
+      configServer.route({
+        method: 'get',
+        path: '/confi',
+        handler(request, reply) {
+          reply(null, {
+            urls: [{
+              name: 'HTTPS Test',
+              url: process.env.HEALTHCHECK_TEST_URL,
+              interval: 'every 2 seconds',
+              expireLimit: 1000 * 60 * 60 * 24 * 1000
+            }]
+          });
+        }
+      });
+      configServer.start(() => {
+        done(null, configServer);
+      });
+    },
+    rapptor(done) {
+      process.env.RAPPTOR_CONFIG_URL = 'http://localhost:3000/confi';
+      const rapptor = new Rapptor();
+      rapptor.start(done);
+    },
+    setup(rapptor, done) {
+      server = rapptor[0];
+      return done(null, rapptor[0]);
+    },
+    report(setup, done) {
+      server.log = (tags, input) => {
+        if (tags.indexOf('HTTPS Test.network.cert') !== -1) {
+          testResults.tagName = true;
+        }
+        if (tags.indexOf('certificate-expiration') !== -1) {
+          testResults.tagCertificate = true;
+        }
+        if (input.message.indexOf('The SSL certificate for') !== -1) {
+          testResults.message = true;
+        }
+      };
+      done();
+    },
+    // wait for it to make calls and log things:
+    wait(report, done) {
+      setTimeout(done, 4000);
+    },
+    cleanup(report, done) {
+      server.methods.methodScheduler.stopSchedule('HTTPS Test.network.cert');
+      server.stop(done);
+    },
+    cleanConfig(cleanup, configServer, done) {
+      configServer.stop(done);
+    },
+  }, () => {
+    t.equal(testResults.tagCertificate, true, 'contains "certificate-expiration" tag');
+    t.equal(testResults.message, true, 'certificate expiration explains what it is warning about');
+    t.end();
+  });
+});
