9931041cc010633f9e0d4b35988c782ed59859a5
Chris <orthagonal1@gmail.com>
2017-11-10 15:25:39 -0600

    add avg/min/max response times

--- routes/api/test.js
+++ routes/api/test.js
@@ -2,14 +2,19 @@ exports.testDown = {
   method: 'GET',
   path: '/api/testDown',
   handler(request, reply) {
-    const result = {
-      timestamp: Date.now(),
-      responseTime: 5000,
-      error: null,
-      up: false,
-      slow: false
+    const data = {
+      averageResponse: 5000,
+      minResponse: 5000,
+      maxResponse: 5000,
+      result: {
+        timestamp: Date.now(),
+        responseTime: 5000,
+        error: 'Response threshold',
+        up: false,
+        slow: true
+      }
     };
-    request.server.log(['service-down', 'blah'], result);
+    request.server.log(['service-down', 'blah'], data);
     reply(null, 'fake service-down log posted, make sure you got a notification for it');
   }
 };
