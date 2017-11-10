f5376613caa22cae1939d713978e81eb8f3212a1
Chris <orthagonal1@gmail.com>
2017-11-10 15:32:15 -0600

    mimick the actual server.log data

--- routes/api/test.js
+++ routes/api/test.js
@@ -3,16 +3,13 @@ exports.testDown = {
   path: '/api/testDown',
   handler(request, reply) {
     const data = {
-      averageResponse: 5000,
-      minResponse: 5000,
-      maxResponse: 5000,
-      result: {
-        timestamp: Date.now(),
-        responseTime: 5000,
-        error: 'Response threshold',
-        up: false,
-        slow: true
-      }
+      type: 'http',
+      name: 'FAKE',
+      url: 'aFakeUrl.com',
+      responseTime: 5000,
+      error: 'Response threshold',
+      retries: 5,
+      message: 'FAKE is down'
     };
     request.server.log(['service-down', 'blah'], data);
     reply(null, 'fake service-down log posted, make sure you got a notification for it');
