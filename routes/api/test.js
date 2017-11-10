279211d14e79c0311e55a58ec538b66fca9ae0ec
Chris <orthagonal1@gmail.com>
2017-11-10 15:20:47 -0600

    fake log object

--- routes/api/test.js
+++ routes/api/test.js
@@ -2,7 +2,14 @@ exports.testDown = {
   method: 'GET',
   path: '/api/testDown',
   handler(request, reply) {
-    request.server.log(['service-down', 'blah'], 'i hate you dad');
+    const result = {
+      timestamp: Date.now(),
+      responseTime: 5000,
+      error: null,
+      up: false,
+      slow: false
+    };
+    request.server.log(['service-down', 'blah'], result);
     reply(null, 'fake service-down log posted, make sure you got a notification for it');
   }
 };
