c92080f163d5ef600492e412f4cecf45c570e9ea
Chris <orthagonal1@gmail.com>
2017-12-12 15:53:56 -0600

    fix local http test

--- test/test.local.js
+++ test/test.local.js
@@ -5,7 +5,6 @@ tap.test('schedules network http', async(t) => {
   const rapptor = new Rapptor({ env: 'test' });
   await rapptor.start();
   const server = rapptor.server;
-  let called = false;
   server.route({
     method: 'get',
     path: '/test/http',
@@ -13,16 +12,13 @@ tap.test('schedules network http', async(t) => {
       return 'hello';
     }
   });
-  let results;
   server.methods.report = async(data, result) => {
-    if (data.name === 'http1' && !called) {
-      results = result;
+    if (data.name === 'http1') {
+      if (result.up) {
+        server.methods.methodScheduler.stopSchedule('http1');
+        await server.stop();
+        t.end();
+      }
     }
   };
-  const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-  await wait(2000);
-  server.methods.methodScheduler.stopSchedule('http1');
-  await server.stop();
-  t.equal(results.up, true);
-  t.end();
 });
