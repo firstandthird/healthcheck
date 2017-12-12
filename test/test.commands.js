9b6de949895877086f079a13e00836b88eaf2076
Chris <orthagonal1@gmail.com>
2017-12-12 15:41:33 -0600

    return help on no input

--- test/test.commands.js
+++ test/test.commands.js
@@ -25,10 +25,10 @@ tap.test('/health (without any text) returns list of options', { timeout: 6000 }
     }
   });
   t.equal(response.result, `Options:
-          status: list last known status for each url
-          check: re-runs health check for all urls
-          [name]: re-runs health check for the specified the url entry
-          certs: re-runs certification check for all urls`);
+        status: list last known status for each url
+        check: re-runs health check for all urls
+        [name]: re-runs health check for the specified the url entry
+        certs: re-runs certification check for all urls`);
   t.end();
 });
 
